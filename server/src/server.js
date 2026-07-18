059105619f8c03fe579cd4359d3aacacff4c12b8
Arpan00095 <arpanmaji2004@gmail.com>
2026-07-18 17:45:32 +0530

    Update server log for production

--- server/src/server.js
+++ server/src/server.js
@@ -6,5 +6,5 @@ const app = require("./app");
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`🚀 Server is running on http://localhost:${PORT}`);
+  console.log(`🚀 Server is running on port ${PORT}`);
 });
